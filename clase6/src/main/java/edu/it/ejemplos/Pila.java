16b56bf13860b3493b7c9be0aa4eab62344a8a6f
andy <arteysoft@gmail.com>
2022-10-03 19:46:59 -0300

    iterar desde -10 hasta +10

--- clase6/src/main/java/edu/it/ejemplos/Pila.java
+++ clase6/src/main/java/edu/it/ejemplos/Pila.java
@@ -5,20 +5,53 @@ public class Pila implements Runnable {
 	public void m6(Integer x) {
 		// Voy a validar si x == 0
 		// eso es porque quiero dividir
+		if (x == 0) {
+			throw new RuntimeException("Atencion que no se puede proseguir... division por zero");
+		}
 		Integer num = 50000 / x;
 		System.out.println("La division es: " + num);
+		for (StackTraceElement elem : new Throwable().getStackTrace()) {
+			//System.out.println("Nombre: " + elem.getMethodName());
+			//System.out.println("Linea: " + elem.getLineNumber());
+		}
 	}
-	// private m5
-	// private m4
-	// private m3
-	// private m2
+	private void m5(Integer x) {
+		System.out.println("Antes de llamar a m6");
+		m6(x + 1);
+		System.out.println("Despues de llamar a m6");
+	}
+	private void m4(Integer x) {
+		System.out.println("Antes de llamar a m5");
+		m5(x + 1);
+		System.out.println("Despues de llamar a m5");
+	}
+	private void m3(Integer x) {
+		System.out.println("Antes de llamar a m4");
+		m4(x + 1);
+		System.out.println("Despues de llamar a m4");
+	}
+	private void m2(Integer x) {
+		System.out.println("Antes de llamar a m3");
+		m3(x + 1);
+		System.out.println("Despues de llamar a m3");
+	}
 	private void m1(Integer x) {
 		System.out.println("Antes de llamar a m2");
-		// m2(x + 1)
+		m2(x + 1);
 		System.out.println("Despues de llamar a m2");
 	}
 	public void run() {
-		System.out.println("Ejercicio de la pila o Stack");
-		m1(1000);
+		try {
+			System.out.println("Ejercicio de la pila o Stack");
+			// Vamos a iterar desde -10 hasta 10 y solo va fallar una de las iteraciones			
+			{
+				m1(-5);
+				Thread.sleep(2000);
+			}
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		System.out.println("Sigue la aplicacion normalmente");
 	}
 }
