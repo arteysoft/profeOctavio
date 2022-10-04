2f24f3124ea5c899b6bba5b6af925d38ab3e739f
andy <arteysoft@gmail.com>
2022-10-03 21:56:52 -0300

    clase #6

--- clase6/src/main/java/edu/it/CursoSpringApplication.java
+++ clase6/src/main/java/edu/it/CursoSpringApplication.java
@@ -2,11 +2,11 @@ package edu.it;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import edu.it.ejemplos.SQL;
+import edu.it.ejemplos.Practica;
 
 @SpringBootApplication
 public class CursoSpringApplication {
 	public static void main(String... params) throws Exception {
-		new SQL().run();
+		new Practica().run();
 	}
 }
--- clase6/src/main/java/edu/it/ejemplos/Practica.java
+++ clase6/src/main/java/edu/it/ejemplos/Practica.java
@@ -1,13 +1,18 @@
 package edu.it.ejemplos;
 
 public class Practica implements Runnable {
-	private void fnThrowChecked() {
-		// Exception
+	private void fnThrowChecked() throws Exception {
+		throw new Exception("Este es un simple Exception");
 	}
 	private void fnThrowUnChecked() {
-		// RuntimeException
+		throw new RuntimeException("Este es un runtime exception");
 	}
 	public void run() {
-		
+		try {
+			fnThrowChecked();
+		}
+		catch (Exception ex) {
+			System.out.println("Ya en el catch... " + ex.getMessage());
+		}
 	}
 }
