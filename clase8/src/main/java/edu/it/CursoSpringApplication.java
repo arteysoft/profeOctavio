83c8af3f77ac44e720c16b5017a57adfe84727f2
andy <arteysoft@gmail.com>
2022-10-24 19:28:46 -0300

    borro clases de la anterior

--- clase8/src/main/java/edu/it/CursoSpringApplication.java
+++ clase8/src/main/java/edu/it/CursoSpringApplication.java
@@ -2,8 +2,6 @@ package edu.it;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import edu.it.colecciones.IngresarDatos;
-
 @SpringBootApplication
 public class CursoSpringApplication {
 	public static void main(String... params) throws Exception {
--- clase8/src/main/java/edu/it/ejemplos/SumaService.java
+++
@@ -1,7 +0,0 @@
-package edu.it.ejemplos;
-
-public class SumaService {
-	public Integer suma(Integer x, Integer y) {
-		return x + y;
-	}
-}
